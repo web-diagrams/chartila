f96e7ddfa506dee07335d8cdd1e45318ebfbe59e
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2023-12-11 22:06:37 +0300

    fix: [build]

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -33,11 +33,11 @@ export function buildPlugins({ mode, paths, analyzer, platform }: BuildOptions):
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }))
-        plugins.push(new CopyPlugin({
-            patterns: [
-                { from: path.resolve(paths.public, 'locales'), to: path.resolve(paths.output, 'locales') },
-            ],
-        }),)
+        // plugins.push(new CopyPlugin({
+        //     patterns: [
+        //         { from: path.resolve(paths.public, 'locales'), to: path.resolve(paths.output, 'locales') },
+        //     ],
+        // }),)
     }
 
     if (analyzer) {
