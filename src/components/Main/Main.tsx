e72ba4d21c22c2460c4e3380099fa57e5aa1f051
yaroslav <yarik545585@gmail.com>
2024-06-01 17:01:14 +0500

    изменнение тайтла + удаление логотипа + переопределение edges

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,20 +1,31 @@
 import 'reactflow/dist/style.css';
 
-import ReactFlow, { Controls, Background, NodeChange, EdgeChange, Connection, BackgroundVariant } from 'reactflow';
+import ReactFlow, {
+  applyEdgeChanges,
+  Background,
+  BackgroundVariant,
+  Connection,
+  Controls,
+  EdgeChange,
+  NodeChange,
+  updateEdge,
+} from 'reactflow';
 import { useAppDispatch, useAppSelector } from '@/app/hooks';
 import Pages from '@/components/Pages/Pages';
 import { NodeTypes } from './interface';
 import StartWindow from '../StartWindow/StartWindow';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { flowActions } from '@/redux/flow/slice/flowSlice';
 import { useCurrentPage } from '@/hooks/useCurrentPage';
 import { NodeData } from '@/redux/flow/constants/constants';
 import Button from '@/shared/Button';
+import { Edge } from '@reactflow/core/dist/esm/types/edges';
 
 function Main() {
   const { pages, currentPageId, selectedNodes } = useAppSelector((state) => state.flow);
   const dispatch = useAppDispatch();
 
+  const edgeUpdateSuccessful = useRef(true);
   const currentPage = useCurrentPage(pages, currentPageId);
 
   const saveToFile = useCallback(() => {
@@ -34,11 +45,38 @@ function Main() {
     URL.revokeObjectURL(href);
   }, [pages, currentPageId]);
 
+  const onEdgeUpdateStart = useCallback(() => {
+    edgeUpdateSuccessful.current = false;
+  }, []);
+
+  const onEdgeUpdate = useCallback(
+    (oldEdge: Edge, newConnection: Connection) => {
+      edgeUpdateSuccessful.current = true;
+      dispatch(flowActions.onChangeEdges(updateEdge(oldEdge, newConnection, currentPage.edges)));
+    },
+    [currentPage],
+  );
+
+  const onEdgeUpdateEnd = useCallback(
+    (_: MouseEvent | TouchEvent, edge: Edge) => {
+      if (!edgeUpdateSuccessful.current) {
+        dispatch(flowActions.onChangeEdges(currentPage.edges.filter((e) => e.id !== edge.id)));
+      }
+
+      edgeUpdateSuccessful.current = true;
+    },
+    [currentPage],
+  );
+
   const onClickOutSide = () => {
     if (selectedNodes.length) {
       dispatch(flowActions.onReleaseNodes());
     }
   };
+
+  useEffect(() => {
+    document.title = pages?.find((page) => page.id === currentPageId)?.pageName ?? 'Web diagrams';
+  }, [currentPageId, pages]);
 
   return (
     <div style={{ height: '100vh', width: '100vw' }} onClick={onClickOutSide}>
@@ -57,11 +95,17 @@ function Main() {
             nodes={currentPage.nodes}
             edges={currentPage.edges}
             onNodesChange={(changes: NodeChange[]) => dispatch(flowActions.onChangeNodes(changes))}
-            onEdgesChange={(changes: EdgeChange[]) => dispatch(flowActions.onChangeEdges(changes))}
+            onEdgesChange={(changes: EdgeChange[]) =>
+              dispatch(flowActions.onChangeEdges(applyEdgeChanges(changes, currentPage.edges)))
+            }
             onConnect={(changes: Connection) => dispatch(flowActions.onConnect(changes))}
             onNodesDelete={(e) => console.log(e)}
+            onEdgeUpdateStart={onEdgeUpdateStart}
+            onEdgeUpdate={onEdgeUpdate}
+            onEdgeUpdateEnd={onEdgeUpdateEnd}
             fitView
             nodeTypes={NodeTypes}
+            proOptions={{ hideAttribution: true }}
           >
             <Background variant={BackgroundVariant.Cross} />
             <Controls />
