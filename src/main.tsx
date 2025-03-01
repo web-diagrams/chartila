d59f6631205275a5a4176f6fa126bacf230f0dfd
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2025-03-01 16:57:59 +0300

    feat: перевел на vite

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import './app/styles/index.scss';
+import { ReactFlowProvider } from 'reactflow';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { routeConfig } from './shared/config/routeConfig';
+
+const root = document.getElementById('root');
+
+if (!root) {
+  throw new Error('root not found');
+}
+
+const container = createRoot(root);
+
+container.render(
+  <Provider store={store}>
+    <ReactFlowProvider>
+      <BrowserRouter>
+        <Routes>
+          {Object.values(routeConfig).map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={route.authOnly ? <div>{route.element}</div> : route.element}
+            />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </ReactFlowProvider>
+  </Provider>,
+);
