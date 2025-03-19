1d5efbf805d2416df54aac0e1e3d6cc131eb25da
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2025-03-19 08:14:18 +0300

    feat: [docs] создание дока без сервера

--- src/hooks/useStartNewDoc.ts
+++ src/hooks/useStartNewDoc.ts
@@ -1,19 +1,30 @@
 import { useCreateDocMutation } from "@/app/api/docsApi";
+import { usePingQuery } from "@/app/api/pingApi";
+import { useAppDispatch } from "@/app/hooks";
 import { getDocPagePath } from "@/shared/config/routePaths";
+import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { v1 } from "uuid";
 
 export const useStartNewDoc = () => {
+  const dispatch = useAppDispatch();
+  const { isError: isServerError } = usePingQuery(undefined);
   const navigate = useNavigate();
   const [createDoc] = useCreateDocMutation();
 
-  const onStartNewProject = async () => {
+  const onStartNewProject = useCallback(async () => {
     const id = v1();
-    const res = await createDoc({ id });
-    if (!('error' in res)) {
+    if (isServerError) {
+      // dispatch()
       navigate(getDocPagePath(id))
+    } else {
+      const res = await createDoc({ id });
+      if (!('error' in res)) {
+        navigate(getDocPagePath(id))
+      }
     }
-  }
+
+  }, [createDoc, navigate, isServerError]);
 
   return {
     onStartNewProject
