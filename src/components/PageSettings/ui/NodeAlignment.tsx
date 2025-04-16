16c744a408d5c4fbb075a81526c51323cf3b9fcb
Oleg Orlov <Dark.eagle217@yandex.ru>
2025-04-16 19:51:41 +0300

    feat: [align] перешел на dagre

--- src/components/PageSettings/ui/NodeAlignment.tsx
+++ src/components/PageSettings/ui/NodeAlignment.tsx
@@ -1,62 +1,56 @@
 import { useCallback } from "react";
-import { useReactFlow, Node, Edge } from "reactflow";
+import { Node, Edge, Position } from "reactflow";
 
-import ELK, { LayoutOptions } from 'elkjs/lib/elk.bundled.js';
 import { useGetDocState } from "@/redux/doc/hooks/useGetDocState";
 import { useCurrentPage } from "@/hooks/useCurrentPage";
 import { useAppDispatch } from "@/app/hooks";
 import { docActions } from "@/redux/doc/slice/docSlice";
 import { cloneDeep } from "lodash";
  
-const elk = new ELK();
- 
-// Elk has a *huge* amount of options to configure. To see everything you can
-// tweak check out:
-//
-// - https://www.eclipse.org/elk/reference/algorithms.html
-// - https://www.eclipse.org/elk/reference/options.html
-const elkOptions = {
-  'elk.algorithm': 'layered',
-  'elk.layered.spacing.nodeNodeBetweenLayers': '100',
-  'elk.spacing.nodeNode': '80',
-};
+import dagre from 'dagre'
 
-const getLayoutedElements = (nodes: Node[], edges: Edge[], options: LayoutOptions = {}) => {
-  const isHorizontal = options?.['elk.direction'] === 'RIGHT';
-  const graph = {
-    id: 'root',
-    layoutOptions: options,
-    children: nodes.map((node) => ({
+const nodeWidth = 172
+const nodeHeight = 36
+
+export function getLayoutedElements(
+  nodes: Node[],
+  edges: Edge[],
+  direction: 'TB' | 'LR' = 'TB' // Top-Bottom или Left-Right
+): { nodes: Node[]; edges: Edge[] } {
+  const isHorizontal = direction === 'LR'
+  const dagreGraph = new dagre.graphlib.Graph()
+
+  dagreGraph.setDefaultEdgeLabel(() => ({}))
+  dagreGraph.setGraph({ rankdir: direction }) // TB = вертикально, LR = горизонтально
+
+  // добавляем узлы
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight })
+  })
+
+  // добавляем связи
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target)
+  })
+
+  dagre.layout(dagreGraph)
+
+  const layoutedNodes = nodes.map((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id)
+    node.targetPosition = isHorizontal ? Position.Left : Position.Top
+    node.sourcePosition = isHorizontal ? Position.Right : Position.Bottom
+
+    return {
       ...node,
-      // Adjust the target and source handle positions based on the layout
-      // direction.
-      targetPosition: isHorizontal ? 'left' : 'top',
-      sourcePosition: isHorizontal ? 'right' : 'bottom',
- 
-      // Hardcode a width and height for elk to use when layouting.
-      width: 150,
-      height: 50,
-    })),
-    edges: edges.map((edge) => ({
-      ...edge,
-      sources: [edge.source],
-      targets: [edge.target],
-    })),
-  };
- 
-  return elk
-  .layout(graph)
-  .then((layoutedGraph) => ({
-    nodes: (layoutedGraph.children ?? []).map((node) => ({
-      ...node,
-      position: { x: node.x, y: node.y },
-      targetPosition: isHorizontal ? 'left' : 'top',
-      sourcePosition: isHorizontal ? 'right' : 'bottom',
-    })),
-    edges: layoutedGraph.edges,
-  }))
-  .catch(console.error);
-};
+      position: {
+        x: nodeWithPosition.x - nodeWidth / 2,
+        y: nodeWithPosition.y - nodeHeight / 2,
+      },
+    }
+  })
+
+  return { nodes: layoutedNodes, edges }
+}
 
 export const NodeAlignment = () => {
     const dispatch = useAppDispatch();
@@ -64,58 +58,48 @@ export const NodeAlignment = () => {
     const currentPage = useCurrentPage(pages, currentPageId);
     
     const onLayout = useCallback(
-        ({ direction }: {direction: string}) => {
+        ({ direction }: {direction: "TB" | "LR"}) => {
             if (!currentPage?.nodes || !currentPage?.edges) {
               return;
             }
 
-            const opts = { 'elk.direction': direction, ...elkOptions };
             const { nodes, edges } = currentPage;
 
-            getLayoutedElements(
-              cloneDeep(nodes.filter((node) => node.selected)), 
+            const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+              cloneDeep(nodes.filter((node) => node.selected)),
               cloneDeep(edges), 
-              opts
-            )
-            .then((data) => {
-                if (!data) {
-                  return;
-                }
-                const { 
-                  nodes: layoutedNodes, 
-                  edges: layoutedEdges 
-                } = data;
-                dispatch(docActions.onSetNodes({ nodes: nodes.map((node) => {
-                  const updatedNode = layoutedNodes.find((lNode) => lNode.id === node.id) 
-                  if (updatedNode) {
-                    return updatedNode as Node;
-                  } else {
-                    return node;
-                  }
-                }) }));
-                if (layoutedEdges) {
-                  const convertedEdges: Edge[] = layoutedEdges.map((elkEdge) => {
-                    const { sources, targets, source, target, ...rest } = elkEdge;
-                    return {
-                        source: sources?.[0] ?? source ?? '',
-                        target: targets?.[0] ?? target ?? '',
-                        ...rest,
-                        // добавь type, label, style и другие поля, если нужно
-                    };
-                  });
-                  dispatch(docActions.onChangeEdges((convertedEdges)));
-                }
-            });
+              direction
+            );
+
+            dispatch(docActions.onSetNodes({ nodes: nodes.map((node) => {
+              const updatedNode = layoutedNodes.find((lNode) => lNode.id === node.id) 
+              if (updatedNode) {
+                return updatedNode as Node;
+              } else {
+                return node;
+              }
+            }) }));
+            // if (layoutedEdges) {
+            //   const convertedEdges: Edge[] = layoutedEdges.map((elkEdge) => {
+            //     return {
+            //         source: sources?.[0] ?? source ?? '',
+            //         target: targets?.[0] ?? target ?? '',
+            //         ...rest,
+            //         // добавь type, label, style и другие поля, если нужно
+            //     };
+            //   });
+            //   dispatch(docActions.onChangeEdges((convertedEdges)));
+            // }
         },
         [currentPage],
       );
 
     const onVertical = () => {
-      onLayout({direction: 'DOWN'})
+      onLayout({direction: 'TB'})
     }
 
     const onHorizontal = () => {
-      onLayout({direction: 'RIGHT'})
+      onLayout({direction: 'LR'})
     }
 
     return (
