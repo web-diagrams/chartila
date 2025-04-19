468d0a4030ce650774f72525abe7e8febde79c5f
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2025-04-19 13:20:29 +0300

    feat: [redesign] back to start button

--- src/pages/DocsPage/ui/DocsPage.tsx
+++ src/pages/DocsPage/ui/DocsPage.tsx
@@ -4,11 +4,18 @@ import styles from './Doc.module.scss';
 import { useStartNewDoc } from "@/hooks/useStartNewDoc";
 import { useCallback } from "react";
 import { useDeleteDocMutation } from "@/app/api/docsApi";
+import { useNavigate } from "react-router-dom";
+import { getStartPath } from "@/shared/config/routePaths";
 
 export const DocsPage = () => {
+  const navigate = useNavigate();
   const { data } = useDocsQuery(undefined);
   const { onStartNewProject } = useStartNewDoc();
   const [deleteDoc, { isLoading }] = useDeleteDocMutation();
+
+  const onBack = () => {
+    navigate(getStartPath());
+  }
 
   const onDeleteDoc = useCallback((id: string) => {
     deleteDoc({ id });
@@ -16,6 +23,7 @@ export const DocsPage = () => {
 
   return (
     <div className={styles.docPage}>
+      <p className={styles.backLink} onClick={onBack}>← Главная страница</p>
       <h1 className={styles.title}>Мои диаграммы</h1>
       <ul className={styles.docList}>
         {data?.map((doc) => (
