ba3c621d0e9f2bc5d07b3d7fff66f57c67836971
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2025-04-19 10:43:31 +0300

    fix: [bugfix] работа с локальным файлом не зависит от сервера

--- src/pages/DocPage/ui/DocPage.tsx
+++ src/pages/DocPage/ui/DocPage.tsx
@@ -4,14 +4,17 @@ import { useServer } from '@/app/providers/ServerProvider/ServerProvider';
 import { ServerDoc } from './ServerDoc/ServerDoc';
 import { LocalDoc } from './LocalDoc/LocalDoc';
 import Pages from '@/components/Pages/Pages';
-import { PageSettings } from '@/components/PageSettings/PageSettings';
+import { useSearchParams } from 'react-router-dom';
 
 export const DocPage = () => {
   const { isServerEnabled } = useServer();
+  const [searchParams] = useSearchParams();
+  // local = true - открываем файл с компьютера
+  const isLocalDoc = searchParams.get('local') === 'true';
 
   return (
     <>
-      {isServerEnabled
+      {(isServerEnabled && !isLocalDoc)
         ? <ServerDoc />
         : <LocalDoc />
       }
